ac5f72e08d7ca777aae9cfdb4e6db20f86ca5009
sebvannistel <sebvannistel@yahoo.fr>
2025-05-24 12:04:37 +0200

    Create details for the get_shared_translation.php
    info example

---
+++ tests/details for the get_shared_translation.php
@@ -0,0 +1,98 @@
+Request on 2025-05-24T09:44:05.487Z
+General:
+Request URL: https://www.subtitlecat.com/get_shared_translation.phpHTTP Version: h3Request method: GETRemote Address: 172.67.172.156
+Headers:
+:authority www.subtitlecat.com
+:method GET
+:path /get_shared_translation.php
+:scheme https
+accept */*
+accept-encoding gzip, deflate, br
+accept-language en-GB,en-US;q=0.9,en;q=0.8
+cookie PHPSESSID=qpfgvrjvp13jt0drahu37gtdf3; _ga=GA1.1.850756534.1741531353; _ga_P8C6PMY45L=GS2.1.s1748079771$o2$g1$t1748079845$j59$l0$h0$dJC0_tvvO8T3RydNaR1Qtp6Kc-6dIYPB5YQ
+referer https://www.subtitlecat.com/subs/17/Aquaman%20Rage%20of%20Atlantis%202018%20720p%20WEB-DL%20H264%20AC3-EVO.html
+sec-ch-ua "Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"
+sec-ch-ua-mobile ?0
+sec-ch-ua-platform "macOS"
+sec-fetch-dest empty
+sec-fetch-mode cors
+sec-fetch-site same-origin
+user-agent Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0
+Query arguments:
+No Get parameters.
+Size:
+Headers: (computed) 1071 bytesBody: 0 bytesTotal: (computed) 1071 bytes
+
+Server's response
+Full response:
+200 OK h3More info about HTTP status codes in Wikipedia .
+Headers:
+alt-svc h3=":443"; ma=86400
+cf-cache-status DYNAMIC
+cf-ray 944bda3aff44b735-BRU
+content-encoding br
+content-type application/json
+date Sat, 24 May 2025 09:44:06 GMT
+nel {"success_fraction":0,"report_to":"cf-nel","max_age":604800}
+report-to {"endpoints":[{"url":"https:\/\/a.nel.cloudflare.com\/report\/v4?s=2k4bRXOTVODCkM7GeA7X2KgK1rR208%2BPV5ccBHtZvngg3EG4NPgxkfrlHSLqbhOWwPPWSMqnigKKVYj%2B2e9ZMjldJQdOv%2BK%2B6EPit1%2BjDozWfFP4Z7k50i0ph%2FkU0mZBmsjXSaDd"}],"group":"cf-nel","max_age":604800}
+server cloudflare
+server-timing cfL4;desc="?proto=QUIC&rtt=24821&min_rtt=13737&rtt_var=8044&sent=1454&recv=423&lost=0&retrans=3&sent_bytes=1526315&recv_bytes=123986&delivery_rate=103878856&cwnd=1536443&unsent_bytes=0&cid=2cefc3bd68ddc38d&ts=75841&x=92"
+Cache:
+Cache data missing
+The cache information is missing from the entry
+
+Size:
+Headers: (computed) 790 bytesBody: (computed) 39 bytesTotal: (computed) 829 bytes
+
+Response data from the server.
+Size:
+39
+Compressed:
+No.
+MIME type:
+application/json
+Content:
+Download the received content.
+
+{
+  "text": null,
+  "language": null,
+  "id": null
+}
+
+Entry cookies
+Cookies sent in the request
+PHPSESSID
+Value qpfgvrjvp13jt0drahu37gtdf3
+Path /
+Domain www.subtitlecat.com
+Expires 1969-12-31T23:59:59.000Z
+_ga
+Value GA1.1.850756534.1741531353
+Path /
+Domain .subtitlecat.com
+Expires 2026-06-28T09:44:05.008Z
+_ga_P8C6PMY45L
+Value GS2.1.s1748079771$o2$g1$t1748079845$j59$l0$h0$dJC0_tvvO8T3RydNaR1Qtp6Kc-6dIYPB5YQ
+Path /
+Domain .subtitlecat.com
+Expires 2026-06-28T09:44:05.008Z
+Cookies received from the server
+No cookies available.
+
+Request times
+‡
+Blocked time
+2 ms
+DNS time
+Does not apply.
+Connection time
+Does not apply.
+Send time
+0 ms
+Wait time
+1058 ms
+Receive time
+1 ms
+SSL time
+Does not apply.
